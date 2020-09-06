8d3611a34c9a6f0bb66117706391f6eb31f65416
toruchan <ishikawatoru01@gmail.com>
2020-09-06 11:44:48 +0900

    チュートリアルの書き方に記述変更

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+
 # from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import include, url
+from django.urls import include, path
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url('polls/', include('polls.urls')),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
